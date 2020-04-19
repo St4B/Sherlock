694dad63bb5908919928db84dcf5c9ed8fe68cb3
Vaios Tsitsonis <vaios.tsitsonis@theblueground.com>
2020-04-19 19:53:52 +0300

    Add dummy tests for capturing screenshots

---
+++ app/src/androidTest/java/com/theblueground/app/MainActivityTest.kt
@@ -0,0 +1,30 @@
+package com.theblueground.app
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.theblueground.sherlock.ViewHierarchyRecorder
+import org.junit.Rule
+import org.junit.Test
+
+class MainActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun a_test_that_takes_a_screenshot_of_the_whole_activity() {
+        activityRule.scenario.onActivity {
+            ViewHierarchyRecorder().record(screenshotName = "main_activity", activity = it)
+        }
+    }
+
+    @Test
+    fun a_test_that_takes_a_screenshot_of_a_specific_view() {
+        activityRule.scenario.onActivity {
+            ViewHierarchyRecorder().record(
+                screenshotName = "main_activity_fab",
+                activity = it,
+                viewId = R.id.fab
+            )
+        }
+    }
+}
