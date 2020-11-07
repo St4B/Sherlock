52a40b40a556199cf5b5c93a871cf2419c386935
Vaios Tsitsonis <vaios.tsitsonis@theblueground.com>
2020-11-07 17:21:35 +0200

    Release sherlock plugin 1.0

--- sherlock-plugin/build.gradle.kts
+++ sherlock-plugin/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("kotlin")
     `java-gradle-plugin`
+    id("com.gradle.plugin-publish") version "0.12.0"
     `maven-publish`
 }
 
@@ -21,5 +22,28 @@ gradlePlugin {
             id = "com.quadible.sherlockplugin"
             implementationClass = "com.quadible.sherlockplugin.SherlockPlugin"
         }
+    }
+}
+
+
+pluginBundle {
+    // These settings are set for the whole plugin bundle
+    website = "https://github.com/St4B/Sherlock"
+    vcsUrl = "https://github.com/St4B/Sherlock"
+
+    (plugins) {
+        "sherlockComposePlugin" {
+            // id is captured from java-gradle-plugin configuration
+            displayName = "Sherlock Gradle Plugin"
+            description = "A plugin that helps us to record screenshots for Android apps (View-Based or Jetpack Compose UIs) and and compare them with previous version to check if unwanted changes were introduced."
+            tags = listOf("Android", "Screenshot", "Testing", "Compose")
+            version = "1.0"
+        }
+    }
+
+    mavenCoordinates {
+        groupId = "com.quadible.sherlockplugin"
+        artifactId = "sherlock-plugin"
+        version = "1.0"
     }
 }
