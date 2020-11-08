26555b25724875a095c2165480d08a037779934b
Vaios Tsitsonis <vaios.tsitsonis@theblueground.com>
2020-11-08 12:39:08 +0200

    Release sherlock 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,9 @@ buildscript {
         classpath("com.android.tools.build:gradle:4.2.0-alpha15")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
         classpath("com.quadible.sherlockplugin:sherlock-plugin:1.0")
+        classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.1")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.10.2")
+
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
